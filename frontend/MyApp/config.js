74124cd26fc57858dc592fe4b485448a34e81db2
Samay Jain <samayjain72258@gmail.com>
2026-03-13 23:16:05 +0530

    Update config.js

--- frontend/MyApp/config.js
+++ frontend/MyApp/config.js
@@ -1,4 +1,4 @@
 // config.js (MyApp/config.js)
-export const BASE_URL = "https://service-desk-backend-2v9vc9hcp-samayjainbms-projects.vercel.app";
+export const BASE_URL = "https://service-desk-backend-sooty.vercel.app";
 export const TOKEN_KEY = "token";
 export const GOOGLE_WEB_CLIENT_ID = "968435682668-68qgqa1hluta32hp26uf400kkh50dbsj.apps.googleusercontent.com";
